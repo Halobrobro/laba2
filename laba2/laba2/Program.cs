48b3d57a33fc0ee14fcc854fb17a8b9b84771923
{Halobrobro} <{Halobrobro@mail.ru}>
2023-04-26 22:34:45 +0700

    1

---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b;
+            char c;
+            double d;
+            Console.WriteLine("Введите А: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Введите B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Введите знак для реализации операции:");
+            Console.WriteLine("(+)");
+            Console.WriteLine("(-)");
+            Console.WriteLine("(*)");
+            Console.WriteLine("(/)");
+            c = Convert.ToChar(Console.ReadLine());
+        }
+    }
+}
